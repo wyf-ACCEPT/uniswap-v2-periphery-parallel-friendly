c4a8a6b75187099e4547fb813080bf27b2f83e6b
wyf-ACCEPT <importwyf@gmail.com>
2025-12-16 17:32:10 +0800

    [feat] update pair init hash for foundry env

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'e4fd61d9099d3704ca9a24fcced1188e8593d767f8bc1b4472c0164c509d4645' // init code hash
             ))));
     }
 
